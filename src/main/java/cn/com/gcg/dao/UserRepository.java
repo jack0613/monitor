8bb795de53f1b03583d6b6d21b8f8d52c28a06b2
wuzs <1696646305@qq.com>
2018-06-05 22:57:45 +0800

    用户DAO

--- src/main/java/cn/com/gcg/dao/UserRepository.java
+++ src/main/java/cn/com/gcg/dao/UserRepository.java
@@ -4,4 +4,6 @@ import cn.com.gcg.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long>{
+
+    public User findByLoginNameAndLoginPwd(String loginName,String loginPwd);
 }
