716b982d48d79a7b2626a9ab1694ac7e284b7197
wuzs <1696646305@qq.com>
2018-06-05 22:58:09 +0800

    登录js

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -5,8 +5,13 @@ $(function () {
     $("#login").click(function () {
         var userName = $("#username").val();
         var passWord = $("#password").val();
+        if(userName ==null || userName =='' || passWord ==null || passWord==''){
+            alert("用户名或密码不能为空");
+            return;
+        }
+        var url = "/user/login/"+userName
         $.ajax({
-            url: "/user/login",
+            url: url,
             data: JSON.stringify(
                 {"username": userName, "password": passWord}
             ),//参数列表,
@@ -14,7 +19,13 @@ $(function () {
             contentType:"application/json",
             dataType:"json",
             success: function (result) {
-                alert("success");
+               // alert("success");
+                if(result.flag=='success'){
+
+                    location.href=""
+                }else{
+                    alert('用户名或密码错误');
+                }
             },
             error: function (result) {
                 alert("登陆失败");
