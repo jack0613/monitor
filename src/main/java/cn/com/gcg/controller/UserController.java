ead3bb272b37abc4d29801f5fc4cc98d15d6cd07
wuzs <1696646305@qq.com>
2018-06-05 22:57:16 +0800

    用户登录数据库校验

--- src/main/java/cn/com/gcg/controller/UserController.java
+++ src/main/java/cn/com/gcg/controller/UserController.java
@@ -3,14 +3,14 @@ package cn.com.gcg.controller;
 import cn.com.gcg.dao.UserRepository;
 import cn.com.gcg.model.User;
 import cn.com.gcg.protocol.MonitorResponse;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Jack on 2018/6/4.
@@ -27,9 +27,23 @@ public class UserController {
     public List<User> getAll(){
         return userRepository.findAll();
     }
-    @RequestMapping(value = "/login",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
-    public MonitorResponse<?> login(@RequestParam( value = "username") String username,@RequestParam(value = "password") String password){
-        return MonitorResponse.success(username + password);
+
+    @RequestMapping(value = "/login/{username}",method = RequestMethod.POST)
+    public Object login(@PathVariable("username") String username, String password, HttpServletRequest request){
+        User user = userRepository.findByLoginNameAndLoginPwd(username,password);
+
+        Map<String,Object> map = new HashMap<String,Object>();
+        if(user ==null){
+            map.put("flag","failed");
+        }else{
+            map.put("flag","success");
+        }
+        return map;
     }
+
+//    @RequestMapping(value = "/login",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+//    public MonitorResponse<?> login(@RequestParam( value = "username") String username,@RequestParam(value = "password") String password){
+//        return MonitorResponse.success(username + password);
+//    }
 
 }
