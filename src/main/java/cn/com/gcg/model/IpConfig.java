accdc3f8a8e4ec89148ae5bb0fd2e35b5c8a7b7d
wuzs <1696646305@qq.com>
2018-06-07 21:28:58 +0800

    删除bootstrap 文件

--- src/main/java/cn/com/gcg/model/IpConfig.java
+++ src/main/java/cn/com/gcg/model/IpConfig.java
@@ -1,29 +1,30 @@
 package cn.com.gcg.model;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
 
 @ToString
 @Data
-@Table(name="monitor_user")
 @Entity
-public class User {
+@Table(name = "monitor_ip")
+public class IpConfig {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name="Login_name")
-    private String loginName;
-    @Column(name="Login_pwd")
-    private String loginPwd;
-    @Column(name="Nick_name")
-    private String nickName;
+
+    @Column(name="ip_addr")
+    private String ipAddr;
+
+    @Column(name="ip_name")
+    private String ipName;
+
+    @Column(name="enabled")
+    private Integer enabled;
+
     @Column(name="areacode")
     private String areacode;
-
 
 }
